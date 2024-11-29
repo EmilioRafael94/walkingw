2efc3f0fe38a8d23e6a3e2c2526786a84d4e35cc
Karlos Hiponia <161108723+Karlos-Hiponia@users.noreply.github.com>
2024-11-29 14:47:15 +0800

    Add files via upload

---
+++ walkingw-master/walkingw-master/resources/views/services.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>Our Services</h1>
+        <p>Explore the services we offer to help you choose the perfect shoes.</p>
+    </div>
+@endsection
